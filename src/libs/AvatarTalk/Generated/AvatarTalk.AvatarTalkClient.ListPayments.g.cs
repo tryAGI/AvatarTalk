672c750b000e2365f7c72cb32bb27ec4b13f1b88
Konstantin S. <havendv@gmail.com>
2026-05-24 10:31:25 +0400

    feat: Updated OpenAPI spec (#19)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/AvatarTalk/Generated/AvatarTalk.AvatarTalkClient.ListPayments.g.cs
+++ src/libs/AvatarTalk/Generated/AvatarTalk.AvatarTalkClient.ListPayments.g.cs
@@ -342,18 +342,17 @@ namespace AvatarTalk
                                     __exception_401 = __ex;
                                 }
 
-                                throw new global::AvatarTalk.ApiException<global::AvatarTalk.ErrorResponse>(
+
+                                throw global::AvatarTalk.ApiException<global::AvatarTalk.ErrorResponse>.Create(
+                                    statusCode: __response.StatusCode,
                                     message: __content_401 ?? __response.ReasonPhrase ?? string.Empty,
                                     innerException: __exception_401,
-                                    statusCode: __response.StatusCode)
-                                {
-                                    ResponseBody = __content_401,
-                                    ResponseObject = __value_401,
-                                    ResponseHeaders = global::System.Linq.Enumerable.ToDictionary(
+                                    responseBody: __content_401,
+                                    responseObject: __value_401,
+                                    responseHeaders: global::System.Linq.Enumerable.ToDictionary(
                                         __response.Headers,
                                         h => h.Key,
-                                        h => h.Value),
-                                };
+                                        h => h.Value));
                             }
 
                             if (__effectiveReadResponseAsString)
@@ -387,17 +386,15 @@ namespace AvatarTalk
                                 }
                                 catch (global::System.Exception __ex)
                                 {
-                                    throw new global::AvatarTalk.ApiException(
+                                    throw global::AvatarTalk.ApiException.Create(
+                                        statusCode: __response.StatusCode,
                                         message: __content ?? __response.ReasonPhrase ?? string.Empty,
                                         innerException: __ex,
-                                        statusCode: __response.StatusCode)
-                                    {
-                                        ResponseBody = __content,
-                                        ResponseHeaders = global::System.Linq.Enumerable.ToDictionary(
+                                        responseBody: __content,
+                                        responseHeaders: global::System.Linq.Enumerable.ToDictionary(
                                             __response.Headers,
                                             h => h.Key,
-                                            h => h.Value),
-                                    };
+                                            h => h.Value));
                                 }
                             }
                             else
@@ -434,17 +431,15 @@ namespace AvatarTalk
                                     {
                                     }
 
-                                    throw new global::AvatarTalk.ApiException(
+                                    throw global::AvatarTalk.ApiException.Create(
+                                        statusCode: __response.StatusCode,
                                         message: __content ?? __response.ReasonPhrase ?? string.Empty,
                                         innerException: __ex,
-                                        statusCode: __response.StatusCode)
-                                    {
-                                        ResponseBody = __content,
-                                        ResponseHeaders = global::System.Linq.Enumerable.ToDictionary(
+                                        responseBody: __content,
+                                        responseHeaders: global::System.Linq.Enumerable.ToDictionary(
                                             __response.Headers,
                                             h => h.Key,
-                                            h => h.Value),
-                                    };
+                                            h => h.Value));
                                 }
                             }
 
